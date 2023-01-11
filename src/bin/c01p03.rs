921f7c25cf1e06ae3a1f7f53f3383909979c3183
SivaKrishna <shivataraya@gmail.com>
2023-01-11 20:31:37 +0530

    Add Chapter 1 Problem 3

---
+++ src/bin/c01p03.rs
@@ -0,0 +1,17 @@
+fn urlify(url: &'static str) -> String {
+    url.trim_end().replace(" ", "%20")
+}
+
+fn main() {
+    println!("Aum Namah Sivaya!!!  {}", urlify("Mr Siva Krishna"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_string_permutation() {
+        assert_eq!(urlify("Mr Siva Krishna"), "Mr%20Siva%20Krishna");
+    }
+}
