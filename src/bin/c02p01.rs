6265c4d1ec1438c23a1e4ce4cf2b00c918d8b932
SivaKrishna <shivataraya@gmail.com>
2023-01-18 19:41:40 +0530

    add a singly linked list

---
+++ src/bin/c02p01.rs
@@ -0,0 +1,66 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type SingleLink = Option<Rc<RefCell<Node>>>;
+
+#[derive(Clone)]
+struct Node {
+    value: String,
+    next: SingleLink,
+}
+
+impl Node {
+    // A nice and short way of creating a new node
+    fn new(value: &str) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(Node {
+            value: value.to_owned(),
+            next: None,
+        }))
+    }
+}
+
+struct TransactionLog {
+    head: SingleLink,
+    tail: SingleLink,
+    pub length: u64,
+}
+
+impl TransactionLog {
+    pub fn new_empty() -> Self {
+        Self {
+            head: None,
+            tail: None,
+            length: 0,
+        }
+    }
+
+    pub fn append(&mut self, value: &str) {
+        let new = Node::new(value);
+        match self.tail.take() {
+            Some(old) => old.borrow_mut().next = Some(new.clone()),
+            None => self.head = Some(new.clone()),
+        };
+        self.length += 1;
+        self.tail = Some(new);
+    }
+
+    pub fn pop(&mut self) -> Option<String> {
+        self.head.take().map(|head| {
+            if let Some(next) = head.borrow_mut().next.take() {
+                self.head = Some(next);
+            } else {
+                self.tail.take();
+            }
+            self.length -= 1;
+            Rc::try_unwrap(head)
+                .ok()
+                .expect("Something is terribly wrong")
+                .into_inner()
+                .value
+        })
+    }
+}
+
+fn main() {
+    println!("Aum Namah sivaya!!!")
+}
